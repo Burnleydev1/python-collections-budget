a9f453cb37fd53ee3835c5389945e1cbe61cf2a2
Abongwa Bonalais Amahnui <abongwabonalais@gmail.com>
2021-11-21 14:56:05 +0100

    corrected some errors

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -3,7 +3,6 @@ from . import Expense
 
 class BudgetList:
 
-
     def __init__(self, budget):
         self.budget = budget
         self.sum_expenses = 0
@@ -13,23 +12,28 @@ class BudgetList:
 
     def append(self, item):
 
-        if self.sum_expenses + item < self.budget:
+        if (self.sum_expenses + item) < self.budget:
             self.expenses.append(item)
-            item += self.sum_expenses
+            self.sum_expenses += item
 
         else:
             self.overages.append(item)
-            item += self.sum_expenses
+            self.sum_overages += item
 
     def __len__(self):
-        len(self.sum_expenses) + len(self.overages)
+        return len(self.expenses) + len(self.overages)
+
 
 def main():
     myBudgetList = BudgetList(1200)
 
     expenses = Expense.Expenses()
-    expenses.read_expenses("data/spending_.csv")
+    expenses.read_expenses("data/spending_data.csv")
 
     for expense in expenses.list:
         myBudgetList.append(expense.amount)
     print(f'The count of all expenses: {str(len(myBudgetList))}')
+
+
+if __name__ == "__main__":
+    main()
