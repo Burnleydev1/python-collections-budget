cfcab129533e62d383ef8fdff16bea81886ab3b8
Abongwa Bonalais Amahnui <abongwabonalais@gmail.com>
2021-11-21 15:32:17 +0100

    added iter and next methods

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -23,6 +23,18 @@ class BudgetList:
     def __len__(self):
         return len(self.expenses) + len(self.overages)
 
+    def __iter__(self):
+        self.iter_e = iter(self.expenses)
+        self.iter_o = iter(self.overages)
+        return self
+
+    def __next__(self):
+        try:
+            return self.iter_e.__next__()
+        except StopIteration as stop:
+            return self.iter_o.__next__()
+
+
 
 def main():
     myBudgetList = BudgetList(1200)
@@ -37,3 +49,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
